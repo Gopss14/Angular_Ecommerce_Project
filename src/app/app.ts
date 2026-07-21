66c3c97d31f68ff5629c4ed1d13a8015b80f5c0c
Gopika I <suba69475@gmail.com>
2026-07-21 16:55:58 +0530

    Day 1-add,edit,update implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,58 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Student} from './student';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-angular-project');
+  name: string = '';
+  age: number = 0;
+  department: string = '';
+  email: string ='';
+  students: Student[] = [];
+  selectedIndex: number = -1;
+
+  addStudent(){
+
+    const student:Student = {
+      name: this.name,
+      age: this.age,
+      department: this.department,
+      email: this.email
+    };
+
+    if(this.selectedIndex === -1){
+      //add new student
+      this.students.push(student);
+    }else{
+      // update existing student
+      this.students[this.selectedIndex] = student;
+      this.selectedIndex = -1;
+    }
+
+    this.name = '';
+    this.age = 0;
+    this.department = '';
+    this.email = '';
+  }
+
+  deleteStudent(index: number){
+    this.students.splice(index, 1);
+  }
+
+  editStudent(index: number){
+    const student = this.students[index];
+    this.name = student.name;
+    this.age = student.age;
+    this.department = student.department;
+    this.email = student.email;
+    this.selectedIndex = index;
+  }
 }
---
+++ src/app/student.ts
@@ -0,0 +1,8 @@
+export interface Student {
+
+  name: string;
+  age: number;
+  department: string;
+  email:string;
+
+}
